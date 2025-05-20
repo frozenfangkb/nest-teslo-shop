8fcf010c3c97a1fa3cdd17a049f7a62afc5da4cb
David Rodriguez Miranda <wolfs_pride@outlook.es>
2025-05-20 12:30:43 +0200

    Seed database

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from 'src/products/products.service';
+import { initialData } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -13,5 +14,31 @@ export class SeedService {
 
   private async insertNewProducts() {
     await this.productsService.deleteAllProducts();
+
+    const products = initialData.products;
+
+    const insertPromises: Promise<
+      | {
+          images: string[];
+          id: string;
+          title: string;
+          price: number;
+          description: string;
+          slug: string;
+          stock: number;
+          sizes: string[];
+          gender: string;
+          tags: string[];
+        }
+      | undefined
+    >[] = [];
+
+    products.forEach((product) => {
+      insertPromises.push(this.productsService.create(product));
+    });
+
+    await Promise.all(insertPromises);
+
+    return true;
   }
 }

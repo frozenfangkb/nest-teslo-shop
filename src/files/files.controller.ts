d94b5fcf4c9bee9a5df4ab6ae33f6ffa7fec9f08
David Rodriguez Miranda <wolfs_pride@outlook.es>
2025-05-21 13:12:37 +0200

    Return secure url on file upload

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -14,10 +14,14 @@ import { fileFilter } from './helpers/file-filter.helper';
 import { diskStorage } from 'multer';
 import { fileNamer } from './helpers/file-namer.helper';
 import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('product/:imageName')
   findProductImage(
@@ -46,7 +50,7 @@ export class FilesController {
       );
     }
 
-    const secureUrl = `${file.filename}`;
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
 
     return {
       secureUrl,

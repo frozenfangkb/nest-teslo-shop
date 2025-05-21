a65412adc572c1e38785f00677f2991874451c96
David Rodriguez Miranda <wolfs_pride@outlook.es>
2025-05-21 10:55:41 +0200

    Upload files to BE filesystem

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -8,6 +8,7 @@ import {
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/file-filter.helper';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
@@ -17,6 +18,9 @@ export class FilesController {
   @UseInterceptors(
     FileInterceptor('file', {
       fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/uploads',
+      }),
     }),
   )
   uploadProductFile(@UploadedFile() file: Express.Multer.File) {

f358838d9e3e749f9c65eddad0a94796075e61f6
David Rodriguez Miranda <wolfs_pride@outlook.es>
2025-05-22 12:12:20 +0200

    Implement JWT Strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,48 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from '../entities/user.entity';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import {
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly configService: ConfigService,
+  ) {
+    const secret: string | undefined = configService.get('JWT_SECRET');
+
+    if (!secret) {
+      console.error('JWT_SECRET is not defined in .env file');
+      throw new InternalServerErrorException(
+        'Internal server error, check server logs',
+      );
+    }
+
+    super({
+      secretOrKey: secret,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { email } = payload;
+
+    const user = await this.userRepository.findOneBy({ email });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    if (!user.isActive) {
+      throw new UnauthorizedException('User is not active');
+    }
+
+    return user;
+  }
+}

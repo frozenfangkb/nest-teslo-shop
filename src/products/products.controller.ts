5d37775aaaa2ba30dda27fc19d6a81d5aafd14b3
David Rodriguez Miranda <wolfs_pride@outlook.es>
2025-05-28 18:44:40 +0200

    add auth to products and seed

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,8 +13,10 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Auth } from '../auth/decorators/auth.decorator';
 
 @Controller('products')
+@Auth()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
